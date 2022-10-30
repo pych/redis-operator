b390332f2cb9b91b8e17c7839b95fa885aff51e0
Samuel Vijaykumar M <mad.vijay@gmail.com>
2022-10-30 21:37:10 +0530

    Add the same as liveness probe to startup as well

--- operator/redisfailover/service/generator.go
+++ operator/redisfailover/service/generator.go
@@ -341,10 +341,19 @@ func generateRedisStatefulSet(rf *redisfailoverv1.RedisFailover, labels map[stri
 								TimeoutSeconds:      5,
 								FailureThreshold:    6,
 								PeriodSeconds:       15,
+								// ProbeHandler: corev1.ProbeHandler{
+								// 	TCPSocket: &corev1.TCPSocketAction{
+								// 		Port: intstr.FromString("redis"),
+								// 		Host: "127.0.0.1",
+								// 	},
+								// },
 								ProbeHandler: corev1.ProbeHandler{
-									TCPSocket: &corev1.TCPSocketAction{
-										Port: intstr.FromString("redis"),
-										Host: "127.0.0.1",
+									Exec: &corev1.ExecAction{
+										Command: []string{
+											"sh",
+											"-c",
+											fmt.Sprintf("redis-cli -h 127.0.0.1 -p %[1]v ping --user pinger --pass pingpass --no-auth-warning", rf.Spec.Redis.Port),
+										},
 									},
 								},
 							},
