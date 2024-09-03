f4cff6baba29e55fa219ab2d04a7576e52da3a57
M Samuel Vijaykumar <mad.vijay@gmail.com>
2024-09-03 23:53:19 +0530

    missed a string

--- test/integration/redisfailover/creation_test.go
+++ test/integration/redisfailover/creation_test.go
@@ -52,7 +52,7 @@ type clients struct {
 	redisClient redis.Client
 }
 
-func (c *clients) prepareNS(currentNamespace) error {
+func (c *clients) prepareNS(currentNamespace sting) error {
 	ns := &corev1.Namespace{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: currentNamespace,
