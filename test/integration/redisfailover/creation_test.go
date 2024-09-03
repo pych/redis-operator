919bc681d99fe8b1210de77a886c5d78b3083271
M Samuel Vijaykumar <mad.vijay@gmail.com>
2024-09-03 23:48:56 +0530

    Testing with different namespaces

--- test/integration/redisfailover/creation_test.go
+++ test/integration/redisfailover/creation_test.go
@@ -37,7 +37,7 @@ import (
 
 const (
 	name           = "testing"
-	namespace      = "rf-integration-tests"
+	namespace      = "testns"
 	redisSize      = int32(3)
 	sentinelSize   = int32(3)
 	authSecretPath = "redis-auth"
@@ -52,24 +52,25 @@ type clients struct {
 	redisClient redis.Client
 }
 
-func (c *clients) prepareNS() error {
+func (c *clients) prepareNS(currentNamespace) error {
 	ns := &corev1.Namespace{
 		ObjectMeta: metav1.ObjectMeta{
-			Name: namespace,
+			Name: currentNamespace,
 		},
 	}
 	_, err := c.k8sClient.CoreV1().Namespaces().Create(context.Background(), ns, metav1.CreateOptions{})
 	return err
 }
 
-func (c *clients) cleanup(stopC chan struct{}) {
-	c.k8sClient.CoreV1().Namespaces().Delete(context.Background(), namespace, metav1.DeleteOptions{})
+func (c *clients) cleanup(stopC chan struct{}, currentNamespace string) {
+	c.k8sClient.CoreV1().Namespaces().Delete(context.Background(), currentNamespace, metav1.DeleteOptions{})
 	close(stopC)
 }
 
 func TestRedisFailover(t *testing.T) {
 	require := require.New(t)
 	disableMyMaster := true
+	currentNamespace := namespace
 
 	// Create signal channels.
 	stopC := make(chan struct{})
@@ -98,7 +99,7 @@ func TestRedisFailover(t *testing.T) {
 	k8sservice := k8s.New(k8sClient, customClient, aeClientset, log.Dummy, metrics.Dummy)
 
 	// Prepare namespace
-	prepErr := clients.prepareNS()
+	prepErr := clients.prepareNS(currentNamespace)
 	require.NoError(prepErr)
 
 	// Give time to the namespace to be ready
@@ -113,7 +114,7 @@ func TestRedisFailover(t *testing.T) {
 	}()
 
 	// Prepare cleanup for when the test ends
-	defer clients.cleanup(stopC)
+	defer clients.cleanup(stopC, currentNamespace)
 
 	// Give time to the operator to start
 	time.Sleep(15 * time.Second)
@@ -133,7 +134,7 @@ func TestRedisFailover(t *testing.T) {
 
 	// Check that if we create a RedisFailover, it is certainly created and we can get it
 	ok := t.Run("Check Custom Resource Creation", func(t *testing.T) {
-		clients.testCRCreation(t, disableMyMaster)
+		clients.testCRCreation(t, currentNamespace, disableMyMaster)
 	})
 	require.True(ok, "the custom resource has to be created to continue")
 
@@ -141,34 +142,45 @@ func TestRedisFailover(t *testing.T) {
 	time.Sleep(3 * time.Minute)
 
 	// Verify that auth is set and actually working
-	t.Run("Check that auth is set in sentinel and redis configs", clients.testAuth)
+	t.Run("Check that auth is set in sentinel and redis configs", func(t *testing.T) {
+		clients.testAuth(t, currentNamespace)
+	})
 
 	// Check custom config is set
-	t.Run("Check that custom config is behave expected", clients.testCustomConfig)
+	t.Run("Check that custom config is behave expected", func(t *testing.T) {
+		clients.testCustomConfig(t, currentNamespace)
+	})
 
 	// Check that a Redis Statefulset is created and the size of it is the one defined by the
 	// Redis Failover definition created before.
-	t.Run("Check Redis Statefulset existing and size", clients.testRedisStatefulSet)
+	t.Run("Check Redis Statefulset existing and size", func(t *testing.T) {
+		clients.testRedisStatefulSet(t, currentNamespace)
+	})
 
 	// Check that a Sentinel Deployment is created and the size of it is the one defined by the
 	// Redis Failover definition created before.
-	t.Run("Check Sentinel Deployment existing and size", clients.testSentinelDeployment)
+	t.Run("Check Sentinel Deployment existing and size", func(t *testing.T) {
+		clients.testSentinelDeployment(t, currentNamespace)
+	})
 
 	// Connect to all the Redis pods and, asking to the Redis running inside them, check
 	// that only one of them is the master of the failover.
-	t.Run("Check Only One Redis Master", clients.testRedisMaster)
+	t.Run("Check Only One Redis Master", func(t *testing.T) {
+		clients.testRedisMaster(t, currentNamespace)
+	})
 
 	// Connect to all the Sentinel pods and, asking to the Sentinel running inside them,
 	// check that all of them are connected to the same Redis node, and also that that node
 	// is the master.
 	t.Run("Check Sentinels Checking the Redis Master", func(t *testing.T) {
-		clients.testSentinelMonitoring(t, disableMyMaster)
+		clients.testSentinelMonitoring(t, currentNamespace, disableMyMaster)
 	})
 }
 
 func TestRedisFailoverMyMaster(t *testing.T) {
 	require := require.New(t)
 	disableMyMaster := false
+	currentNamespace := "mymaster-" + namespace
 
 	// Create signal channels.
 	stopC := make(chan struct{})
@@ -197,14 +209,14 @@ func TestRedisFailoverMyMaster(t *testing.T) {
 	k8sservice := k8s.New(k8sClient, customClient, aeClientset, log.Dummy, metrics.Dummy)
 
 	// Prepare namespace
-	prepErr := clients.prepareNS()
+	prepErr := clients.prepareNS(currentNamespace)
 	require.NoError(prepErr)
 
 	// Give time to the namespace to be ready
 	time.Sleep(15 * time.Second)
 
 	// Create operator and run.
-	redisfailoverOperator, err := redisfailover.New(redisfailover.Config{}, k8sservice, k8sClient, namespace, redisClient, metrics.Dummy, log.Dummy)
+	redisfailoverOperator, err := redisfailover.New(redisfailover.Config{}, k8sservice, k8sClient, currentNamespace, redisClient, metrics.Dummy, log.Dummy)
 	require.NoError(err)
 
 	go func() {
@@ -212,7 +224,7 @@ func TestRedisFailoverMyMaster(t *testing.T) {
 	}()
 
 	// Prepare cleanup for when the test ends
-	defer clients.cleanup(stopC)
+	defer clients.cleanup(stopC, currentNamespace)
 
 	// Give time to the operator to start
 	time.Sleep(15 * time.Second)
@@ -221,18 +233,18 @@ func TestRedisFailoverMyMaster(t *testing.T) {
 	secret := &v1.Secret{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      authSecretPath,
-			Namespace: namespace,
+			Namespace: currentNamespace,
 		},
 		Data: map[string][]byte{
 			"password": []byte(testPass),
 		},
 	}
-	_, err = k8sClient.CoreV1().Secrets(namespace).Create(context.Background(), secret, metav1.CreateOptions{})
+	_, err = k8sClient.CoreV1().Secrets(currentNamespace).Create(context.Background(), secret, metav1.CreateOptions{})
 	require.NoError(err)
 
 	// Check that if we create a RedisFailover, it is certainly created and we can get it
 	ok := t.Run("Check Custom Resource Creation", func(t *testing.T) {
-		clients.testCRCreation(t, disableMyMaster)
+		clients.testCRCreation(t, currentNamespace, disableMyMaster)
 	})
 	require.True(ok, "the custom resource has to be created to continue")
 
@@ -240,32 +252,42 @@ func TestRedisFailoverMyMaster(t *testing.T) {
 	time.Sleep(3 * time.Minute)
 
 	// Verify that auth is set and actually working
-	t.Run("Check that auth is set in sentinel and redis configs", clients.testAuth)
+	t.Run("Check that auth is set in sentinel and redis configs", func(t *testing.T) {
+		clients.testAuth(t, currentNamespace)
+	})
 
 	// Check custom config is set
-	t.Run("Check that custom config is behave expected", clients.testCustomConfig)
+	t.Run("Check that custom config is behave expected", func(t *testing.T) {
+		clients.testCustomConfig(t, currentNamespace)
+	})
 
 	// Check that a Redis Statefulset is created and the size of it is the one defined by the
 	// Redis Failover definition created before.
-	t.Run("Check Redis Statefulset existing and size", clients.testRedisStatefulSet)
+	t.Run("Check Redis Statefulset existing and size", func(t *testing.T) {
+		clients.testRedisStatefulSet(t, currentNamespace)
+	})
 
 	// Check that a Sentinel Deployment is created and the size of it is the one defined by the
 	// Redis Failover definition created before.
-	t.Run("Check Sentinel Deployment existing and size", clients.testSentinelDeployment)
+	t.Run("Check Sentinel Deployment existing and size", func(t *testing.T) {
+		clients.testSentinelDeployment(t, currentNamespace)
+	})
 
 	// Connect to all the Redis pods and, asking to the Redis running inside them, check
 	// that only one of them is the master of the failover.
-	t.Run("Check Only One Redis Master", clients.testRedisMaster)
+	t.Run("Check Only One Redis Master", func(t *testing.T) {
+		clients.testRedisMaster(t, currentNamespace)
+	})
 
 	// Connect to all the Sentinel pods and, asking to the Sentinel running inside them,
 	// check that all of them are connected to the same Redis node, and also that that node
 	// is the master.
 	t.Run("Check Sentinels Checking the Redis Master", func(t *testing.T) {
-		clients.testSentinelMonitoring(t, disableMyMaster)
+		clients.testSentinelMonitoring(t, currentNamespace, disableMyMaster)
 	})
 }
 
-func (c *clients) testCRCreation(t *testing.T, args ...bool) {
+func (c *clients) testCRCreation(t *testing.T, currentNamespace string, args ...bool) {
 	disableMyMaster := false
 	if len(args) > 0 && args[0] {
 		disableMyMaster = args[0]
@@ -274,7 +296,7 @@ func (c *clients) testCRCreation(t *testing.T, args ...bool) {
 	toCreate := &redisfailoverv1.RedisFailover{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      name,
-			Namespace: namespace,
+			Namespace: currentNamespace,
 		},
 		Spec: redisfailoverv1.RedisFailoverSpec{
 			Redis: redisfailoverv1.RedisSettings{
@@ -294,39 +316,39 @@ func (c *clients) testCRCreation(t *testing.T, args ...bool) {
 		},
 	}
 
-	c.rfClient.DatabasesV1().RedisFailovers(namespace).Create(context.Background(), toCreate, metav1.CreateOptions{})
-	gotRF, err := c.rfClient.DatabasesV1().RedisFailovers(namespace).Get(context.Background(), name, metav1.GetOptions{})
+	c.rfClient.DatabasesV1().RedisFailovers(currentNamespace).Create(context.Background(), toCreate, metav1.CreateOptions{})
+	gotRF, err := c.rfClient.DatabasesV1().RedisFailovers(currentNamespace).Get(context.Background(), name, metav1.GetOptions{})
 
 	assert.NoError(err)
 	assert.Equal(toCreate.Spec, gotRF.Spec)
 }
 
-func (c *clients) testRedisStatefulSet(t *testing.T) {
+func (c *clients) testRedisStatefulSet(t *testing.T, currentNamespace string) {
 	assert := assert.New(t)
-	redisSS, err := c.k8sClient.AppsV1().StatefulSets(namespace).Get(context.Background(), fmt.Sprintf("rfr-%s", name), metav1.GetOptions{})
+	redisSS, err := c.k8sClient.AppsV1().StatefulSets(currentNamespace).Get(context.Background(), fmt.Sprintf("rfr-%s", name), metav1.GetOptions{})
 	assert.NoError(err)
 	assert.Equal(redisSize, int32(redisSS.Status.Replicas))
 }
 
-func (c *clients) testSentinelDeployment(t *testing.T) {
+func (c *clients) testSentinelDeployment(t *testing.T, currentNamespace string) {
 	assert := assert.New(t)
-	sentinelD, err := c.k8sClient.AppsV1().Deployments(namespace).Get(context.Background(), fmt.Sprintf("rfs-%s", name), metav1.GetOptions{})
+	sentinelD, err := c.k8sClient.AppsV1().Deployments(currentNamespace).Get(context.Background(), fmt.Sprintf("rfs-%s", name), metav1.GetOptions{})
 	assert.NoError(err)
 	assert.Equal(3, int(sentinelD.Status.Replicas))
 }
 
-func (c *clients) testRedisMaster(t *testing.T) {
+func (c *clients) testRedisMaster(t *testing.T, currentNamespace string) {
 	assert := assert.New(t)
 	masters := []string{}
 
-	redisSS, err := c.k8sClient.AppsV1().StatefulSets(namespace).Get(context.Background(), fmt.Sprintf("rfr-%s", name), metav1.GetOptions{})
+	redisSS, err := c.k8sClient.AppsV1().StatefulSets(currentNamespace).Get(context.Background(), fmt.Sprintf("rfr-%s", name), metav1.GetOptions{})
 	assert.NoError(err)
 
 	listOptions := metav1.ListOptions{
 		LabelSelector: labels.FormatLabels(redisSS.Spec.Selector.MatchLabels),
 	}
 
-	redisPodList, err := c.k8sClient.CoreV1().Pods(namespace).List(context.Background(), listOptions)
+	redisPodList, err := c.k8sClient.CoreV1().Pods(currentNamespace).List(context.Background(), listOptions)
 
 	assert.NoError(err)
 
@@ -340,7 +362,7 @@ func (c *clients) testRedisMaster(t *testing.T) {
 	assert.Equal(1, len(masters), "only one master expected")
 }
 
-func (c *clients) testSentinelMonitoring(t *testing.T, args ...bool) {
+func (c *clients) testSentinelMonitoring(t *testing.T, currentNamespace string, args ...bool) {
 	disableMyMaster := false
 	if len(args) > 0 {
 		disableMyMaster = args[0]
@@ -354,13 +376,13 @@ func (c *clients) testSentinelMonitoring(t *testing.T, args ...bool) {
 	assert := assert.New(t)
 	masters := []string{}
 
-	sentinelD, err := c.k8sClient.AppsV1().Deployments(namespace).Get(context.Background(), fmt.Sprintf("rfs-%s", name), metav1.GetOptions{})
+	sentinelD, err := c.k8sClient.AppsV1().Deployments(currentNamespace).Get(context.Background(), fmt.Sprintf("rfs-%s", name), metav1.GetOptions{})
 	assert.NoError(err)
 
 	listOptions := metav1.ListOptions{
 		LabelSelector: labels.FormatLabels(sentinelD.Spec.Selector.MatchLabels),
 	}
-	sentinelPodList, err := c.k8sClient.CoreV1().Pods(namespace).List(context.Background(), listOptions)
+	sentinelPodList, err := c.k8sClient.CoreV1().Pods(currentNamespace).List(context.Background(), listOptions)
 	assert.NoError(err)
 
 	for _, pod := range sentinelPodList.Items {
@@ -378,15 +400,15 @@ func (c *clients) testSentinelMonitoring(t *testing.T, args ...bool) {
 	assert.True(isMaster, "Sentinel should monitor the Redis master")
 }
 
-func (c *clients) testAuth(t *testing.T) {
+func (c *clients) testAuth(t *testing.T, currentNamespace string) {
 	assert := assert.New(t)
 
-	redisCfg, err := c.k8sClient.CoreV1().ConfigMaps(namespace).Get(context.Background(), fmt.Sprintf("rfr-%s", name), metav1.GetOptions{})
+	redisCfg, err := c.k8sClient.CoreV1().ConfigMaps(currentNamespace).Get(context.Background(), fmt.Sprintf("rfr-%s", name), metav1.GetOptions{})
 	assert.NoError(err)
 	assert.Contains(redisCfg.Data["redis.conf"], "requirepass "+testPass)
 	assert.Contains(redisCfg.Data["redis.conf"], "masterauth "+testPass)
 
-	redisSS, err := c.k8sClient.AppsV1().StatefulSets(namespace).Get(context.Background(), fmt.Sprintf("rfr-%s", name), metav1.GetOptions{})
+	redisSS, err := c.k8sClient.AppsV1().StatefulSets(currentNamespace).Get(context.Background(), fmt.Sprintf("rfr-%s", name), metav1.GetOptions{})
 	assert.NoError(err)
 
 	assert.Len(redisSS.Spec.Template.Spec.Containers, 2)
@@ -401,16 +423,16 @@ func (c *clients) testAuth(t *testing.T) {
 	assert.Equal(redisSS.Spec.Template.Spec.Containers[1].Env[4].ValueFrom.SecretKeyRef.LocalObjectReference.Name, authSecretPath)
 }
 
-func (c *clients) testCustomConfig(t *testing.T) {
+func (c *clients) testCustomConfig(t *testing.T, currentNamespace string) {
 	assert := assert.New(t)
 
-	redisSS, err := c.k8sClient.AppsV1().StatefulSets(namespace).Get(context.Background(), fmt.Sprintf("rfr-%s", name), metav1.GetOptions{})
+	redisSS, err := c.k8sClient.AppsV1().StatefulSets(currentNamespace).Get(context.Background(), fmt.Sprintf("rfr-%s", name), metav1.GetOptions{})
 	assert.NoError(err)
 
 	listOptions := metav1.ListOptions{
 		LabelSelector: labels.FormatLabels(redisSS.Spec.Selector.MatchLabels),
 	}
-	redisPodList, err := c.k8sClient.CoreV1().Pods(namespace).List(context.Background(), listOptions)
+	redisPodList, err := c.k8sClient.CoreV1().Pods(currentNamespace).List(context.Background(), listOptions)
 	assert.NoError(err)
 
 	rClient := rediscli.NewClient(&rediscli.Options{
